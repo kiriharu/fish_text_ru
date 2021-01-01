cf325d28500e5ffe6d39f50f5fb72f9fc04ec18b
kiriharu <kiriharu@yandex.ru>
2021-01-01 19:30:06 +0300

    html wrapper, rewrited process response in json wrapper

--- fishtext/api.py
+++ fishtext/api.py
@@ -29,7 +29,7 @@ class FishTextAPI:
     def process_response(self, response) -> None:
         raise NotImplementedError
 
-    def get(self):
+    def get(self, number: int = 100):
         raise NotImplementedError
 
 
@@ -48,6 +48,9 @@ class FishTextJson(FishTextAPI):
 
     def process_response(self, response: JsonAPIResponse) -> None:
 
+        if response.errorCode is None:
+            return
+
         error_codes = {
             11: TooManyContentExceeded,
             21: CallLimitExceeded,
@@ -55,10 +58,9 @@ class FishTextJson(FishTextAPI):
             31: InternalServerError
         }
 
-        if response.errorCode is not None:
-            exception = error_codes.get(response.errorCode, None)
-            if exception:
-                raise exception(response.text)
+        exception = error_codes.get(response.errorCode, None)
+        if exception:
+            raise exception(response.text)
 
     def get(self, number: int = 100) -> JsonAPIResponse:
         json_response = self.session.get(
@@ -77,6 +79,8 @@ class FishTextJson(FishTextAPI):
 
 class FishTextHtml(FishTextAPI):
 
+    TOO_MUCH_CONTENT_EXCEEDED = "You requested too much content. Be more moderate."
+
     def __init__(self, session: Optional[Session] = None,
                  api_url: str = FISH_TEXT_API_URL,
                  text_type: TextType = TextType.Sentence
@@ -87,3 +91,21 @@ class FishTextHtml(FishTextAPI):
             text_type=text_type,
             text_format=TextFormat.html
         )
+
+    def process_response(self, response: Response) -> None:
+        if response.text == self.TOO_MUCH_CONTENT_EXCEEDED:
+            raise TooManyContentExceeded(response.text)
+        if response.status_code == 403:
+            raise CallLimitExceeded(response.text)
+            # TODO: дописать BannedForever
+        if response.status_code == 500:
+            raise InternalServerError(response.text)
+
+    def get(self, number: int = 100) -> str:
+        response = self.session.get(
+            FISH_TEXT_API_URL, params=dict(
+                format=self.text_format, number=number, type=self.text_type
+            )
+        )
+        self.process_response(response)
+        return response.text
