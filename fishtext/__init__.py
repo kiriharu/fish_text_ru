aed8c04e6cd1b276ae1e3ac9e2bad11b2a388b60
Maxim Mosin <max@mosin.pw>
2022-08-25 21:48:44 +0900

    Ignore ImportError if httpx is not installed.

--- fishtext/__init__.py
+++ fishtext/__init__.py
@@ -1,5 +1,8 @@
 __version__ = "2.1.0"
 from .api import FishTextJson, FishTextHtml, FishTextAPI
-from .async_api import (
-    AsyncFishTextJson, AsyncFishTextHtml, AsyncFishTextAPI
-)
+try:
+    from .async_api import (
+        AsyncFishTextJson, AsyncFishTextHtml, AsyncFishTextAPI
+    )
+except ImportError:
+    pass
